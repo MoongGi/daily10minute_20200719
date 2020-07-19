ac2d405e5b194090a3c1020610ab7b0647096e76
김문기 <heartboy08@nate.com>
2020-07-19 14:51:38 +0900

    회원가입 프로세스

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/SignUpActvity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/SignUpActvity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_sign_up_actvity.*
 
@@ -18,21 +19,7 @@ class SignUpActvity : BaseActivity() {
 
     override fun setValues()
     {
-        //EditText (비번 입력칸) 에 글자를 타이핑하는 이벤트 체크
-        pwEdt.addTextChangedListener(object : TextWatcher
-        {
-            override fun afterTextChanged(s: Editable?) {
-               // TODO("Not yet implemented")
-            }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                checkPasswords()
-            }
-        })
     }
 
     fun checkPasswords()
@@ -59,5 +46,58 @@ class SignUpActvity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        //회원 가입 버튼 누르면 빈 입력값 있는지 확인후
+        //괜찮으면 실제로 데이터 요청
+
+        okBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+            val inputNickName = nickEdt.text.toString()
+
+            if (inputEmail.isEmpty()) {
+                Toast.makeText(mContext, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show()
+                // 가입 절차 강제 종료
+                return@setOnClickListener
+            } else if (!inputEmail.contains("@")) {
+                Toast.makeText(mContext, "이메일 양식이 아닙니다. ", Toast.LENGTH_SHORT).show()
+                // 가입 절차 강제 종료
+                return@setOnClickListener
+            }
+
+            if (inputPw.length < 8) {
+                Toast.makeText(mContext, "비밀번호가 너무 짧습니다. ", Toast.LENGTH_SHORT).show()
+                // 가입 절차 강제 종료
+                return@setOnClickListener
+            }
+
+            if (inputNickName.isEmpty())
+            {
+                Toast.makeText(mContext, "닉네임을 입력해주세요. ", Toast.LENGTH_SHORT).show()
+                // 가입 절차 강제 종료
+                return@setOnClickListener
+            }
+
+            //이메일 / 비번 / 닉네임 검사를 모두 통과한 상황
+            //서버에 실제로 가입 신청
+
+        }
+
+        //EditText (비번 입력칸) 에 글자를 타이핑하는 이벤트 체크
+        pwEdt.addTextChangedListener(object : TextWatcher
+        {
+            override fun afterTextChanged(s: Editable?) {
+                // TODO("Not yet implemented")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkPasswords()
+            }
+        })
     }
 }
