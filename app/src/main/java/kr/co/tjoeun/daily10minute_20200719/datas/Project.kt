20cb2e986752bd1be637f6d7286a00edbf09a699
moong <hihibibi00@naver.com>
2020-07-25 10:17:13 +0900

    프로젝트 정보와 목록에 필요한 데이터베이스 / XML 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/datas/Project.kt
@@ -0,0 +1,10 @@
+package kr.co.tjoeun.daily10minute_20200719.datas
+
+class Project
+{
+    //15.164.153.174/api/docs
+    var id = 0
+    var title = ""
+    var imageUrl = ""
+    var description = ""
+}
